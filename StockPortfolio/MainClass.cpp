e841f8f7594bfe13c37cda6b54c4c9b20dd03a99
CNGUYEN <CNGUYEN@ERC108-07>
2017-02-17 13:31:45 -0500

    Test project from class.

---
+++ StockPortfolio/MainClass.cpp
@@ -0,0 +1,19 @@
+#include "MainClass.h"
+#include <iostream>
+using namespace std;
+
+MainClass::MainClass()
+{
+}
+
+MainClass::~MainClass()
+{
+}
+
+
+double computeStock()
+
+int main()
+{
+	//
+}
